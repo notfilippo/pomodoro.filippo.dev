d84f366f900cbe4a213f472ac5e6de0768f14a02
Filippo <filipporossi@hey.com>
2024-01-26 22:02:34 +0100

    feat: initial commit

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,31 +1,41 @@
 import { useParams } from "react-router-dom";
 
-import * as Y from "yjs";
-import { WebrtcProvider } from "y-webrtc";
-import { useCallback, useEffect, useState } from "react";
-import { Button, ButtonGroup, Center, Flex, Text } from "@chakra-ui/react";
+import { useCallback, useEffect, useReducer, useState } from "react";
+import {
+  Button,
+  ButtonGroup,
+  Center,
+  Flex,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 
-interface Timer {
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+
+type Timer = {
   expiration: number;
   running: boolean;
   session: number;
   focus: number;
   break: number;
   longBreak: number;
-}
+};
 
 const DEFAULT_TIMER: Timer = {
   expiration: 0,
   running: false,
   session: 0,
-  focus: 25 * 60,
-  break: 5 * 60,
-  longBreak: 15 * 60,
+  focus: 25, // * 60,
+  break: 5, // * 60,
+  longBreak: 15, // * 60,
 };
 
 const CORRECTION_FACTOR = 1100;
 
-function getSessionTime(timer: Timer): number {
+function getSessionTime(timer: Readonly<Timer> | null): number {
+  if (!timer) return 0;
+
   if ((timer.session + 1) % 4 === 0) {
     return timer.longBreak;
   } else if ((timer.session + 1) % 2 === 0) {
@@ -35,7 +45,9 @@ function getSessionTime(timer: Timer): number {
   return timer.focus;
 }
 
-function getSessionName(timer: Timer): string {
+function getSessionName(timer: Readonly<Timer> | null): string {
+  if (!timer) return "Focus";
+
   if ((timer.session + 1) % 4 === 0) {
     return "Long Break";
   } else if ((timer.session + 1) % 2 === 0) {
@@ -48,107 +60,125 @@ function getSessionName(timer: Timer): string {
 export default function Room() {
   const { id } = useParams();
 
-  const [map, setMap] = useState<Y.Map<unknown> | null>(null);
+  const [ytimer, setYTimer] = useState<Y.Map<unknown> | null>(null);
+  const [timer, setTimer] = useState({ ...DEFAULT_TIMER });
 
-  const [timer, setTimerNative] = useState<Timer>({ ...DEFAULT_TIMER });
-
-  const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  const setTimer = useCallback(
-    (t: Timer | ((t: Timer) => Timer)) => {
-      setTimerNative((old) => {
-        const timer = typeof t === "function" ? t(old) : t;
-        map?.set("expiration", timer.expiration);
-        map?.set("running", timer.running);
-        map?.set("session", timer.session);
-        map?.set("focus", timer.focus);
-        map?.set("break", timer.break);
-        map?.set("longBreak", timer.longBreak);
-        return timer;
+  useEffect(() => {
+    if (!id) return;
+    const ydoc = new Y.Doc();
+
+    console.log("connecting to", import.meta.env.VITE_WEBSOCKET_PROVIDER);
+    const yprovider = new WebsocketProvider(
+      import.meta.env.VITE_WEBSOCKET_PROVIDER,
+      id,
+      ydoc,
+    );
+
+    const ytimer = ydoc.getMap("timer");
+    ytimer.observeDeep(() => {
+      console.log("timer changed", ytimer.toJSON());
+      setTimer({ ...DEFAULT_TIMER, ...ytimer.toJSON() });
+    });
+
+    setYTimer(ytimer);
+
+    return () => {
+      yprovider.destroy();
+      ydoc.destroy();
+      setYTimer(null);
+    };
+  }, [id]);
+
+  const [, forceUpdate] = useReducer((x: number) => x + 1, 0);
+
+  useEffect(() => {
+    document.addEventListener("visibilitychange", forceUpdate);
+    return () => document.removeEventListener("visibilitychange", forceUpdate);
+  }, [forceUpdate]);
+
+  const updateTimer = useCallback(
+    (fn: (timer: Timer) => Timer) => {
+      setTimer((timer) => {
+        const updatedTimer = fn(timer);
+        ytimer?.set("expiration", updatedTimer.expiration);
+        ytimer?.set("running", updatedTimer.running);
+        ytimer?.set("session", updatedTimer.session);
+        ytimer?.set("focus", updatedTimer.focus);
+        ytimer?.set("break", updatedTimer.break);
+        ytimer?.set("longBreak", updatedTimer.longBreak);
+        return updatedTimer;
       });
     },
-    [map],
+    [ytimer],
   );
 
+  const startTimer = () => {
+    const expiration =
+      Date.now() + getSessionTime(timer) * 1000 + CORRECTION_FACTOR;
+    updateTimer((timer) => ({
+      ...timer,
+      expiration,
+      running: true,
+    }));
+  };
+
+  const resetTimer = () => {
+    updateTimer((timer) => ({
+      ...timer,
+      expiration: 0,
+      running: false,
+    }));
+  };
+
+  const stopAndAdvance = useCallback(() => {
+    updateTimer((timer) => ({
+      ...timer,
+      session: timer.session + 1,
+      expiration: 0,
+      running: false,
+    }));
+  }, [updateTimer]);
+
   const tick = useCallback(() => {
-    if (!timer.running) return;
+    if (!timer) return;
 
     const remaining = timer.expiration - Date.now();
 
-    setHours(Math.max(Math.floor(remaining / 1000 / 60 / 60), 0));
     setMinutes(Math.max(Math.floor((remaining / 1000 / 60) % 60), 0));
     setSeconds(Math.max(Math.floor((remaining / 1000) % 60), 0));
 
-    if (remaining <= 0) {
-      setTimer((timer) => ({
-        ...timer,
-        running: false,
-        session: timer.session + 1,
-      }));
+    if (remaining <= 0 && timer.running) {
+      stopAndAdvance();
     }
-  }, [timer, setTimer]);
+  }, [timer, stopAndAdvance]);
 
   useEffect(() => {
     const interval = setInterval(tick, 200);
     return () => clearInterval(interval);
   }, [tick]);
 
-  useEffect(() => {
-    if (!id) return;
-
-    const ydoc = new Y.Doc();
-    new WebrtcProvider(id ?? "default", ydoc);
-
-    const ymap = ydoc.getMap("start");
-
-    ymap.observe(() => {
-      setTimerNative({
-        expiration: Number(ymap.get("expiration")) ?? DEFAULT_TIMER.expiration,
-        running: Boolean(ymap.get("running")) ?? DEFAULT_TIMER.running,
-        session: Number(ymap.get("session")) ?? DEFAULT_TIMER.session,
-        focus: Number(ymap.get("focus")) ?? DEFAULT_TIMER.focus,
-        break: Number(ymap.get("break")) ?? DEFAULT_TIMER.break,
-        longBreak: Number(ymap.get("longBreak")) ?? DEFAULT_TIMER.longBreak,
-      });
-    });
-
-    setMap(ymap);
-
-    return () => ydoc.destroy();
-  }, [id, setTimerNative]);
-
-  const startTimer = () => {
-    const next = getSessionTime(timer) * 1000 + CORRECTION_FACTOR;
-    setTimer((timer) => ({
-      ...timer,
-      expiration: Date.now() + next,
-      running: true,
-    }));
-  };
-
-  const stopTimer = () => {
-    setTimer((timer) => ({
-      ...timer,
-      running: false,
-    }));
-  };
-
   return (
     <Center h="100vh">
       <Flex direction="column" alignItems="center" gap={2}>
         <Text>
-          {getSessionName(timer)} {timer.session + 1}
+          {getSessionName(timer)} ({timer.session})
         </Text>
         <Text fontSize="6xl" fontFamily="'Azeret Mono', monospace">
-          {hours.toString().padStart(2, "0")}:
           {minutes.toString().padStart(2, "0")}:
           {seconds.toString().padStart(2, "0")}
         </Text>
         <ButtonGroup>
-          <Button onClick={startTimer}>Start</Button>
-          <Button onClick={stopTimer}>Stop</Button>
+          <Button onClick={startTimer} isDisabled={timer?.running}>
+            Start
+          </Button>
+          <Tooltip label="⚠️ This will reset the timer for everyone">
+            <Button onClick={resetTimer} isDisabled={!timer?.running}>
+              Reset
+            </Button>
+          </Tooltip>
         </ButtonGroup>
       </Flex>
     </Center>
